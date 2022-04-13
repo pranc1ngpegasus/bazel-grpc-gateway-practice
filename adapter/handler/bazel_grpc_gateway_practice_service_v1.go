8b0dd1cb4667fa59211a3c4af4c20bf77a615b7e
Pranc1ngPegasus <ride.or.die.2215@icloud.com>
2022-04-13 20:32:19 +0900

    BazelGrpcGatewayPracticeServiceV1 handlerをinterfaceにした

--- adapter/handler/bazel_grpc_gateway_practice_service_v1.go
+++ adapter/handler/bazel_grpc_gateway_practice_service_v1.go
@@ -4,10 +4,16 @@ import (
 	pb "github.com/Pranc1ngPegasus/bazel-grpc-gateway-practice/proto/bazel_grpc_gateway_practice/v1"
 )
 
-type bazelGrpcGatewayPracticeServiceV1 struct {
-	pb.UnimplementedBazelGrpcGatewayPracticeServiceServer
-}
+var _ pb.BazelGrpcGatewayPracticeServiceServer = (*bazelGrpcGatewayPracticeServiceV1)(nil)
 
-func NewBazelGrpcGatewayPracticeServiceV1() pb.BazelGrpcGatewayPracticeServiceServer {
+type (
+	BazelGrpcGatewayPracticeServiceV1 pb.BazelGrpcGatewayPracticeServiceServer
+
+	bazelGrpcGatewayPracticeServiceV1 struct {
+		pb.UnimplementedBazelGrpcGatewayPracticeServiceServer
+	}
+)
+
+func NewBazelGrpcGatewayPracticeServiceV1() BazelGrpcGatewayPracticeServiceV1 {
 	return &bazelGrpcGatewayPracticeServiceV1{}
 }
