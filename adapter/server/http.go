409f7024cf04a0b6c919f44d09d41e6778986c3d
Pranc1ngPegasus <ride.or.die.2215@icloud.com>
2022-04-23 10:07:52 +0900

    envoyサーバをadapter層に移植してDIした

---
+++ adapter/server/http.go
@@ -0,0 +1,59 @@
+package server
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/Pranc1ngPegasus/bazel-grpc-gateway-practice/adapter/configuration"
+	pb "github.com/Pranc1ngPegasus/bazel-grpc-gateway-practice/proto/bazel_grpc_gateway_practice/v1"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type (
+	HttpServer interface {
+		Serve() error
+	}
+
+	httpServer struct {
+		server http.Handler
+		config configuration.Config
+	}
+)
+
+func NewHttpServer(
+	config configuration.Config,
+) HttpServer {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(
+			insecure.NewCredentials(),
+		),
+	}
+
+	if err := pb.RegisterBazelGrpcGatewayPracticeServiceHandlerFromEndpoint(ctx, mux, ":"+config.Grpc.ServerPort, opts); err != nil {
+		log.Error().Stack().Caller().Err(err)
+		panic(err)
+	}
+
+	return &httpServer{
+		server: mux,
+	}
+}
+
+func (s *httpServer) Serve() error {
+	log.Info().Msgf("HTTP port: %s", s.config.Http.ServerPort)
+	if err := http.ListenAndServe(":"+s.config.Http.ServerPort, s.server); err != nil {
+		log.Error().Stack().Caller().Err(err)
+		return err
+	}
+
+	return nil
+}
--- cmd/envoy/main.go
+++ cmd/envoy/main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"context"
-	"net/http"
-
 	"github.com/Pranc1ngPegasus/bazel-grpc-gateway-practice/adapter/configuration"
-	pb "github.com/Pranc1ngPegasus/bazel-grpc-gateway-practice/proto/bazel_grpc_gateway_practice/v1"
 
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/rs/zerolog/log"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 func init() {
@@ -18,26 +11,10 @@ func init() {
 }
 
 func main() {
-	env := configuration.Get()
+	server := initialize()
 
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
-	mux := runtime.NewServeMux()
-	opts := []grpc.DialOption{
-		grpc.WithTransportCredentials(
-			insecure.NewCredentials(),
-		),
-	}
-
-	if err := pb.RegisterBazelGrpcGatewayPracticeServiceHandlerFromEndpoint(ctx, mux, ":"+env.Grpc.ServerPort, opts); err != nil {
-		log.Error().Err(err)
-		return
-	}
-
-	if err := http.ListenAndServe(":"+env.Http.ServerPort, mux); err != nil {
-		log.Error().Err(err)
+	if err := server.Serve(); err != nil {
+		log.Error().Err(err).Caller().Stack()
 		return
 	}
 }
